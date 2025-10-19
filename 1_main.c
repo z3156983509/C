0cab9b1023303c4ebc8f3d7652b11f028694dfee
z3156983509 <3156983509@qq.com>
2025-10-20 00:56:03 +0800

    Update main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,14 @@
-//
-//
-//
-#include <stdio.h>
+//202510302105
+//3156983509@qq.com
+//周娜
+#include<stdio.h>
 int main()
-{
-    printf("Hello World");
+{  float temperatureC,tempertureF;
+    printf("Please input the temperature in Celsius.\n");
+    scanf("%f",& temperatureC);
+    tempertureF=temperatureC*1.8+32;
+    printf("%f inCelsius is equal to %.1f in Fahrenheit.\n",temperatureC,tempertureF);
+
     return 0;
+
 }
