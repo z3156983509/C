0a7b76e09d63973d2a9655a6dee789bb479e9349
z3156983509 <3156983509@qq.com>
2025-10-21 15:36:19 +0800

    Update main.c
    Fix syntax errors and improve grade calculation logic.

---
+++ 2_main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(void)
+{print("请输入该学生的整数成绩");
+scanf("%d",&score);
+char grade;
+if(score>=90&&score<=100)
+{
+    printf("grade=A");
+}
+eles if(score>=80&&score<90)
+{
+    printf("grade=B");
+}
+eles if(score>=70&&score<80)
+{
+    printf("grade=C");
+}
+eles if(score>=60&&score<70)
+{
+    ptintf("grade=D");
+}
+else
+{
+    print("grade=E")
+}
+return 0;
+}
