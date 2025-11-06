24e335f361d9a33db681b0455e78eb7cf815ff2f
z3156983509 <3156983509@qq.com>
2025-11-06 23:16:49 +0800

    Update 7_main.c

--- 7_main.c
+++ 7_main.c
@@ -2,23 +2,30 @@
 //3156983509@qq.com
 //周娜
 #include<stdio.h>
+void function(int arr[],int size)
+{
+    int temp=0;
+    for(int i=0;i<size/2;i++)
+    {
+        temp=arr[i];
+        arr[i]=arr[size-1-i];
+        arr[size-1-i]=temp;
+    }
+}
 int main()
 {
     int arr[5];
-    for(int i=0;i<4;i++)
+    for (int i=0;i<5;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(int i=4;i>0;i--)
-    arr[i]=arr[i-1];
-    arr[0]=0;
+    function(arr,5);
     for(int i=0;i<5;i++)
     {
-      printf("%d",arr[i]);
-      if(i<4)
-      {
-        printf(" ");
-      }
+        printf("%d",arr[i]);
+        if(i<4){
+            printf(" ");
+        }
     }
     return 0;
 }
