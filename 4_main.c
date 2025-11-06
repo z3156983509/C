1087fe5ffaeb49a0459aaba7e9963baf305b9f94
z3156983509 <3156983509@qq.com>
2025-11-06 23:14:50 +0800

    Update 4_main.c
    Refactor main function and add power function for exponentiation.

--- 4_main.c
+++ 4_main.c
@@ -2,19 +2,24 @@
 //3156983509@qq.com
 //周娜
 #include<stdio.h>
+double power(double a,int b)
+{
+    double result=1.0;
+    int i;
+    for (int i=0;i<b;i++)
+    {
+        result*=a;
+    }
+    return result;
+}
 int main()
-{ int x,y,z;
-int i=100;
-while(i<999&&i>=100)
 {
-i++;
-x=i/100;
-y=(i/10)%10;
-z=i%10;
-if(i==x*x*x+y*y*y+z*z*z)
-{
-    printf ("%d ",i);
-}
-}
+    double sum=0;
+    int i;
+    for(i=1;i<=5;i++)
+    {
+        sum+=power(i,2);
+    }
+    printf("%.0f\n",sum);
     return 0;
 }
