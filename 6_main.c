3f65d372957b203bad6d71905697e53a77599258
z3156983509 <3156983509@qq.com>
2025-11-06 23:16:12 +0800

    Update 6_main.c
    Refactor main function and implement sum and product calculations for an array.

--- 6_main.c
+++ 6_main.c
@@ -2,30 +2,33 @@
 //3156983509@qq.com
 //周娜
 #include<stdio.h>
+int sumarray(int arr[],int b)
+{
+    int sum=0;
+    for(int i=0;i<b;i++)
+    {
+        sum+=arr[i];
+    }
+    return sum;
+}
+int productarray(int arr[],int c)
+{
+    int product=1;
+    for (int i=0;i<c;i++)
+    {
+        product*=arr[i];
+    }
+    return product;
+}
 int main()
 {
     int arr[5];
-    for(int i=0;i<5;i++)
-    {
-        int num;
-        while(1)
-        {
-        scanf("%d",&num);
-        if(num%2==0)
-        {
-            arr[i]=num;
-            break;
-        }
-    }
-  }
-  for(int i=0;i<5;i++)
-  {
-    printf("%d",arr[i]);
-    if(i!=4)
-    {
-        printf(" ");
-    }
-    
-  }
-  return 0;
+for(int i=0;i<5;i++)
+{
+    scanf("%d",&arr[i]);
+}
+int sum=sumarray(arr,5);
+int product=productarray(arr,5);
+printf("%d %d",sum,product);
+return 0;
 }
