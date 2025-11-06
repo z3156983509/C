f7bfd34bc80b5fef737ec9c454857454e02bcf0a
z3156983509 <3156983509@qq.com>
2025-11-06 23:20:15 +0800

    Update main.c

--- 1_main.c
+++ 1_main.c
@@ -10,8 +10,8 @@ int main ()
         scanf("%d",&a[i][j]);
             }
         }
-for(int j=0;j<3;j++){
-    for(int i=0;i<3;i++){
+for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
         printf("%d ",a[i][j]);
     }
     printf("\n");
